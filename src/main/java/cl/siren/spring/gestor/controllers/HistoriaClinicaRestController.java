12e7b9c0d4b4e61ecb93435008886e6480da0320
Joss <jocelynreyeslobos@gmail.com>
2023-08-17 05:32:42 -0400

    Paso 4

--- src/main/java/cl/siren/spring/gestor/controllers/HistoriaClinicaRestController.java
+++ src/main/java/cl/siren/spring/gestor/controllers/HistoriaClinicaRestController.java
@@ -1,17 +1,15 @@
 package cl.siren.spring.gestor.controllers;
 
-import javax.persistence.PostUpdate;
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import cl.siren.spring.gestor.model.RespuestaAPI;
 import cl.siren.spring.gestor.model.entity.HistoriaClinica;
 import cl.siren.spring.gestor.model.services.HistoriaClinicaService;
 
@@ -32,6 +29,12 @@ public class HistoriaClinicaRestController {
 
 	@Autowired
 	HistoriaClinicaService historiaClinicaService;
+	
+	@GetMapping
+	public ResponseEntity<String> status(){
+		LocalDateTime timestamp = LocalDateTime.now();
+		return ResponseEntity.status(HttpStatus.OK).body(timestamp+ ": API corriendo");
+	}
 	
 	@PostMapping(
 		path = "/crear",
@@ -63,55 +66,4 @@ public class HistoriaClinicaRestController {
 		return ResponseEntity.status(HttpStatus.OK).body("Hstoria clinica eliminada");
 		
 	}
-	
-	
-    // Constantes
-    // -----------------------------------------------------------------------------------------
-
-    /** Objeto {@link Logger} que contiene los métodos de depuración */
-    private static final Logger logger = LogManager.getLogger(HistoriaClinicaRestController.class);
-
-    // Solicitudes GET
-    // -----------------------------------------------------------------------------------------
-
-    /**
-     * Hola Mundo!
-     * 
-     * @param request objeto {@link HttpServletRequest} que contiene la información
-     *                de la solicitud que le hace el cliente al {@link HttpServlet}
-     * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
-     */
-    @GetMapping
-    public ResponseEntity<RespuestaAPI> mostrarMensaje(HttpServletRequest request) {
-        // Depuración
-        logger.info("[API] Solicitud GET: {}", request.getRequestURI());
-
-        // Crear respuesta
-        RespuestaAPI respuesta = new RespuestaAPI(HttpStatus.OK, "mostrarMensaje", "Hola Mundo!");
-
-        // Devolver respuesta
-        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
-    }
-
-    /**
-     * Hola Mundo! (Personalizado)
-     * 
-     * @param nombre  nombre de la persona
-     * @param request objeto {@link HttpServletRequest} que contiene la información
-     *                de la solicitud que le hace el cliente al {@link HttpServlet}
-     * @return un objeto {@link ResponseEntity} con la respuesta a la solicitud
-     */
-    @GetMapping(path = "/{nombre}")
-    public ResponseEntity<RespuestaAPI> mostrarMensajePersonalizado(@PathVariable String nombre, HttpServletRequest request) {
-        // Depuración
-        logger.info("[API] Solicitud GET: {}", request.getRequestURI());
-
-        // Crear respuesta
-        RespuestaAPI respuesta = new RespuestaAPI(HttpStatus.OK, "mostrarMensajePersonalizado",
-                "Hola, " + nombre + "!");
-
-        // Devolver respuesta
-        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
-    }
-
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="core" uri="http://java.sun.com/jsp/jstl/core" %>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <!DOCTYPE html>
 <html>
     <!-- Cabecera -->
@@ -12,7 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Título de la página -->
-        <title>Hola mundo!</title>
+        <title>Historias Clinicas</title>
 
         <!-- Hojas de Estilo -->
         <link rel="stylesheet" href="${pageContext.request.contextPath}/res/css/estilo.css">
@@ -21,10 +22,15 @@
 
     <!-- Cuerpo -->
     <body>
-        <h1>Hola mundo!</h1>
-        <p>
-            Bienenidos sean todos!
-        </p>
+        <div class="my-5">
+		  <div class="p-5 text-center bg-body-tertiary">
+		    <div class="container py-5">
+		      <h1 class="text-body-emphasis">Jocelyn Reyes Lobos</h1>
+		      <p class="col-lg-8 mx-auto lead">
+		        Web responsiva<code> a cada resolución</code>, API corriend en /api/historiaClinica</p>
+		    </div>
+		  </div>
+		</div>
     </body>
     <!-- /Cuerpo -->
 </html>
